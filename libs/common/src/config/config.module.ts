55631501a8239db98baf6d69b835726be8153289
Milind Jamnekar <milindjamnekar502@gmail.com>
2025-03-24 08:33:06 +0530

    add common library with database and config modules

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestjsConfigModule,
+} from '@nestjs/config';
+import * as joi from 'joi';
+
+@Module({
+  imports: [
+    NestjsConfigModule.forRoot({
+      validationSchema: joi.object({
+        MONGODB_URL: joi.string(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
