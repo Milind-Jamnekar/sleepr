bb199e86508604f103d2881525ba883345203b96
Milind Jamnekar <milindjamnekar502@gmail.com>
2025-08-24 07:56:59 +0530

    fix: update import path for DatabaseModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule } from '@app/common/database';
 
 @Module({
   imports: [DatabaseModule],
